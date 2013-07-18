632cd17caeaa8fb7e3922c3571f462033b4ea23e
Callum Rogers <callumrogers@hotmail.co.uk>
2013-07-18 23:30:10 +0100

    Initial Commit

---
+++ EmptyDllMain/EmptyDllMain.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+BOOL APIENTRY DllMain( HANDLE hModule, 
+                   DWORD  ul_reason_for_call, 
+                   LPVOID lpReserved
+                 )
+{
+    return TRUE;
+}
